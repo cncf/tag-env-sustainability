299e8589e4e6669ec4d568e2c61d065730dd77ac
Azanul <azanulhaque@gmail.com>
2023-08-06 01:25:37 +0530

    unwanted error
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- checks/spelling.sh
+++ checks/spelling.sh
@@ -5,4 +5,13 @@ set -o pipefail
 
 npm install -g cspell
 # git fetch origin main:main
-git diff --name-only --diff-filter=AM main $HEAD | grep '\.md$' | xargs --no-run-if-empty -L1 npx cspell -c ./checks/spelling-config.json
+changed_files=$(git diff --name-only --diff-filter=AM main $HEAD | grep '\.md$' || true)
+
+if [ -n "$changed_files" ]; then
+    echo "Running spelling check on the following files:"
+    echo "$changed_files"
+    echo "--------------------------------------------"
+    echo "$changed_files" | xargs --no-run-if-empty -L1 npx cspell -c ./checks/spelling-config.json
+else
+    echo "No markdown files changed. Skipping spelling check."
+fi
