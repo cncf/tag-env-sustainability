c562159af2cdb4b5c8b8efeb2d36111059065b8a
Kristina Devochko <guidemetothemoon@gmail.com>
2023-11-10 20:04:50 +0100

    chore: refactor QA GitHub Worfklow
    Signed-off-by: Kristina Devochko <guidemetothemoon@gmail.com>

--- checks/links.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-npm list markdown-link-check || npm install markdown-link-check
-for file_name in $(git diff --name-only $HEAD main | grep '\.md$'); do
-  npx markdown-link-check --config ./checks/link-config.json --progress --verbose "$file_name"
-done
