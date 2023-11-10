1fbe50f963367a7468d29a1e869df674919dadea
Kristina Devochko <guidemetothemoon@gmail.com>
2023-11-10 20:04:50 +0100

    chore: refactor QA GitHub Worfklow
    Signed-off-by: Kristina Devochko <guidemetothemoon@gmail.com>

--- checks/spelling.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-npm install -g cspell
-git fetch origin main:main
-git diff --name-only --diff-filter=AM main $HEAD | grep '\.md$' | xargs --no-run-if-empty -L1 npx cspell -c ./checks/spelling-config.json
