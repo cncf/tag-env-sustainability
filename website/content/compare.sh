a6e05962a40d4c464f9aacf03f7c9c4557af063b
Sergio Méndez <sergioarm.gpl@gmail.com>
2024-04-03 23:01:54 -0600

    feat: mod script
    Signed-off-by: Sergio Méndez <sergioarm.gpl@gmail.com>

--- website/content/compare.sh
+++ website/content/compare.sh
@@ -1,5 +1,13 @@
 OUTPUT_DIR="./outdated"
 
+# Make an output directory
+if [[ ! -e $OUTPUT_DIR ]]; then
+    mkdir $OUTPUT_DIR
+    #echo "${L10N_INFO_JSON}" > ${OUTPUT_DIR}/L10N_INFO.json
+elif [[ ! -d $OUTPUT_DIR ]]; then
+    echo "$OUTPUT_DIR already exists but is not a directory" 1>&2
+fi
+
 LATEST_BRANCH=env-sus-changes #${GITHUB_REF#refs/}
 echo "(DEBUG) LATEST_BRANCH: ${LATEST_BRANCH}"
 
@@ -12,14 +20,18 @@ compare () {
         # Set output directory
     FILE=$1
     echo "comparing "$FILE
-    sleep 3
+    #sleep 3
     # Actually compare between the old and latest English content and log diff in the file
     if [[ -f "${FILE}" ]]; then
         # File exists
         # Check changes
+        echo "running diff"
+        echo "${OLD_BRANCH}..${LATEST_BRANCH} -- ${FILE}"
         git diff ${OLD_BRANCH}..${LATEST_BRANCH} -- ${FILE} > temp.diff
+        #sleep 2
         if [[ -s "temp.diff" ]]; then
             echo "(DEBUG) ${FILE} is outdated."
+            #sleep 2
             mv temp.diff ${OUTPUT_DIR}/${FILE}
         fi
     else
